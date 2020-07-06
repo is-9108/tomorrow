9a881bba975162684cbac16e1393181f474c2440
is-9108 <syouta103010@gmail.com>
2020-07-06 23:39:14 +0900

    first commit

---
+++ tomorowTodo/todoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  todoViewController.swift
+//  tomorowTodo
+//
+//  Created by Shota Ishii on 2020/07/06.
+//  Copyright © 2020 is. All rights reserved.
+//
+
+import UIKit
+
+class todoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
